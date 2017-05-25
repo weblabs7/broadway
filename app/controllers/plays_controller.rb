4ed3d8647dcefbb3bfb870a84ed4c6c9dde0b3a4
weblabs7 <weblabs@live.com>
2017-05-25 20:15:53 +0000

    added the user id to the database. Added the user id assocated with the controller.

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -19,11 +19,11 @@ def update
 end
 
 def new
-    @play = Play.new
+    @play = current_user.play.build
 end
 
 def create
-    @play = Play.new(play_params)
+    @play = current_user.play.build(play_params)
     
     if @play.save
         redirect_to root_path
